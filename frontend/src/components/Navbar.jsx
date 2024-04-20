452a8faac843b5eac5a07be431a855d9e6aa0053
navod <navodsachintha@gmail.com>
2024-04-20 12:44:58 +0530

    changed nav bar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -38,22 +38,22 @@ const Nav = () => {
   };
 
   // return (
-  //   <nav class=" bg-black dark:bg-gray-900 fixed w-full z-20 top-0 start-0  dark:border-gray-600">
-  //     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  //       <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+  //   <nav className=" bg-black dark:bg-gray-900 fixed w-full z-20 top-0 start-0  dark:border-gray-600">
+  //     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  //       <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
   //         <img
   //           src={logo}
-  //           class="w-9 h-9 object-contain bg-white rounded-full"
+  //           className="w-9 h-9 object-contain bg-white rounded-full"
   //           alt="TurboRaceInsight Logo"
   //         />
-  //         <span class="self-center hidden md:flex text-2xl font-semibold whitespace-nowrap text-white">
+  //         <span className="self-center hidden md:flex text-2xl font-semibold whitespace-nowrap text-white">
   //           TurboRaceInsight
   //         </span>
-  //         <span class="self-center flex md:hidden text-2xl font-semibold whitespace-nowrap text-white">
+  //         <span className="self-center flex md:hidden text-2xl font-semibold whitespace-nowrap text-white">
   //           TRInsignt
   //         </span>
   //       </a>
-  //       <div class="flex lg:order-2 gap-5 space-x-3 lg:space-x-0 rtl:space-x-reverse">
+  //       <div className="flex lg:order-2 gap-5 space-x-3 lg:space-x-0 rtl:space-x-reverse">
   //         {isEmptyObject(userData) ? (
   //           <>
   //             <Link
@@ -80,7 +80,7 @@ const Nav = () => {
   //           // </div>
   //           <Dropdown
   //             label={userData.first_name}
-  //             class="bg-black lg:flex hidden text-white font-bold capitalize text-2xl border-2 rounded-md px-4"
+  //             className="bg-black lg:flex hidden text-white font-bold capitalize text-2xl border-2 rounded-md px-4"
   //             dismissOnClick={false}
   //           >
   //             {userData.role === "ADMIN" && (
@@ -95,13 +95,13 @@ const Nav = () => {
   //         <button
   //           data-collapse-toggle="navbar-sticky"
   //           type="button"
-  //           class="inline-flex   items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+  //           className="inline-flex   items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
   //           aria-controls="navbar-sticky"
   //           aria-expanded="false"
   //         >
-  //           <span class="sr-only">Open main menu</span>
+  //           <span className="sr-only">Open main menu</span>
   //           <svg
-  //             class="w-5 h-5"
+  //             className="w-5 h-5"
   //             aria-hidden="true"
   //             xmlns="http://www.w3.org/2000/svg"
   //             fill="none"
@@ -116,14 +116,14 @@ const Nav = () => {
   //         </button>
   //       </div>
   //       <div
-  //         class="items-center shadow-md justify-between hidden w-full lg:flex lg:w-auto lg:order-1"
+  //         className="items-center shadow-md justify-between hidden w-full lg:flex lg:w-auto lg:order-1"
   //         id="navbar-sticky"
   //       >
-  //         <ul class="flex flex-col p-4 lg:p-0 mt-4 font-medium border border-gray-100 rounded-lg lg:space-x-8 rtl:space-x-reverse lg:flex-row lg:mt-0 lg:border-0 dark:bg-gray-800 lg:dark:bg-gray-400 dark:border-gray-700">
+  //         <ul className="flex flex-col p-4 lg:p-0 mt-4 font-medium border border-gray-100 rounded-lg lg:space-x-8 rtl:space-x-reverse lg:flex-row lg:mt-0 lg:border-0 dark:bg-gray-800 lg:dark:bg-gray-400 dark:border-gray-700">
   //           <li>
   //             <a
   //               href="#"
-  //               class="block py-2 px-3 text-white bg-blue-700 rounded lg:bg-transparent lg:text-blue-700 lg:p-0 lg:dark:text-blue-500"
+  //               className="block py-2 px-3 text-white bg-blue-700 rounded lg:bg-transparent lg:text-blue-700 lg:p-0 lg:dark:text-blue-500"
   //               aria-current="page"
   //             >
   //               Home
@@ -132,7 +132,7 @@ const Nav = () => {
   //           <li>
   //             <a
   //               href="#upcomingrace"
-  //               class="block py-2 px-3 text-white rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0 lg:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+  //               className="block py-2 px-3 text-white rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0 lg:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
   //             >
   //               Races
   //             </a>
@@ -140,7 +140,7 @@ const Nav = () => {
   //           <li>
   //             <a
   //               href="#homedescription"
-  //               class="block py-2 px-3 text-white rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0 lg:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+  //               className="block py-2 px-3 text-white rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0 lg:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
   //             >
   //               About
   //             </a>
@@ -149,7 +149,7 @@ const Nav = () => {
   //           <li>
   //             <a
   //               href="#footer"
-  //               class="block py-2 px-3 text-white rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0 lg:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+  //               className="block py-2 px-3 text-white rounded hover:bg-gray-100 lg:hover:bg-transparent lg:hover:text-blue-700 lg:p-0 lg:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
   //             >
   //               Contact
   //             </a>
@@ -170,7 +170,7 @@ const Nav = () => {
   //             ) : (
   //               <Dropdown
   //                 label={userData.first_name}
-  //                 class="bg-black lg:hidden text-white font-bold capitalize text-2xl"
+  //                 className="bg-black lg:hidden text-white font-bold capitalize text-2xl"
   //                 dismissOnClick={false}
   //               >
   //                 {userData.role == "ADMIN" && (
@@ -195,31 +195,31 @@ const Nav = () => {
   // );
 
   return (
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a
           href="https://flowbite.com/"
-          class="flex items-center space-x-3 rtl:space-x-reverse"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
         >
           <img
             src="https://flowbite.com/docs/images/logo.svg"
-            class="h-8"
+            className="h-8"
             alt="Flowbite Logo"
           />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             Flowbite
           </span>
         </a>
         <button
           data-collapse-toggle="navbar-default"
           type="button"
-          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-default"
           aria-expanded="false"
         >
-          <span class="sr-only">Open main menu</span>
+          <span className="sr-only">Open main menu</span>
           <svg
-            class="w-5 h-5"
+            className="w-5 h-5"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -234,12 +234,12 @@ const Nav = () => {
             />
           </svg>
         </button>
-        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <a
                 href="#"
-                class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                 aria-current="page"
               >
                 Home
@@ -248,7 +248,7 @@ const Nav = () => {
             <li>
               <a
                 href="#"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 About
               </a>
@@ -256,7 +256,7 @@ const Nav = () => {
             <li>
               <a
                 href="#"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Services
               </a>
@@ -264,7 +264,7 @@ const Nav = () => {
             <li>
               <a
                 href="#"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Pricing
               </a>
@@ -272,7 +272,7 @@ const Nav = () => {
             <li>
               <a
                 href="#"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Contact
               </a>
